fba4c0fb59f8f3ab7a57066775ed67ade1469ee9
Ricardo Yang <ricardo.yang00@gmail.com>
2024-11-09 17:39:11 +0000

    corrected efficiency formula

--- src/statistics.c
+++ src/statistics.c
@@ -7,7 +7,7 @@ double timeDiff(struct timeval start, struct timeval end) {
 
 // a
 double propagation_to_transmission_ratio(int baudrate, int maxPayload) {
-    return (double) ((TPROPAGATION / 1000) / (maxPayload * 8 / baudrate));
+    return ((double) TPROPAGATION / 1000.0) / ((double) maxPayload * 8.0 / (double) baudrate);
 }
 
 double received_bit_rate(Statistics stats) {
@@ -17,16 +17,19 @@ double received_bit_rate(Statistics stats) {
 // FER = P(bcc1 error) + P(bcc2 error) * (1 - P(bcc1 error))
 // probability of any error in either BCC1 or BCC2
 double fer() {
-    return (double) (BCC1_ERROR) / 100 + (BCC2_ERROR / 100) * (1 - BCC1_ERROR / 100);
+    double bcc1_error_rate = (double) BCC1_ERROR / 100.0;
+    double bcc2_error_rate = (double) BCC2_ERROR / 100.0;
+    return bcc1_error_rate + bcc2_error_rate * (1 - bcc1_error_rate);
 }
 
 // Optimal Efficiency = (1 - FER) / (1 + 2a)
 double optimal_efficiency(int baudrate, int maxPayload) {
-    return (double) (1 - fer() / (1 + 2 * propagation_to_transmission_ratio(baudrate, maxPayload)));
+    double fer_value = fer();
+    double a = propagation_to_transmission_ratio(baudrate, maxPayload);
+    return (1 - fer_value) / (1 + 2 * a);
 }
 
 // Actual Efficiency =  Actual Received Bitrate / Link Capacity
 double actual_efficiency(Statistics stats, int baudrate) {
-    //printf("Received Bit Rate: %f\n", received_bit_rate(stats));
     return (double) (received_bit_rate(stats) / baudrate);
 }
